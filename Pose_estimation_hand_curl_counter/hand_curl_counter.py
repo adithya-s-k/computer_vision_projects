ce61f7d6db3e2150f2a65d1200fb00fb7631cd08
Adithya S K <adithyaskolavi@gmail.com>
2021-12-20 20:23:16 +0530

    Curl counter for both hands done💪 - Adithya S

--- Pose_estimation_hand_curl_counter/hand_curl_counter.py
+++ Pose_estimation_hand_curl_counter/hand_curl_counter.py
@@ -1,11 +1,15 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-
-
-
+import time
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
+
+inputGoal = int(input("Enter your rep goal for each arm: "))
+
+cap = cv2.VideoCapture(0)
+# cap.set(3,1280)
+# cap.set(4,960)
 
 def calculate_angle(a,b,c):
     a = np.array(a) # First
@@ -14,19 +18,16 @@ def calculate_angle(a,b,c):
     
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
-    
     if angle >180.0:
-        angle = 360-angle
-        
+        angle = 360-angle    
     return angle 
 
-cap = cv2.VideoCapture(0)
-cap.set(3,10000)
-cap.set(4,10000)
 
 # Curl counter variables
 counter = 0 
+counter_r = 0
 stage = None
+stage_r = None
 
 ## Setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
@@ -50,50 +51,81 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             
             # Get coordinates
             shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            print(shoulder)
             elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            print(elbow)
             wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-            print(wrist)
-            
             # Calculate angle
             angle = calculate_angle(shoulder, elbow, wrist)
+            
+            
+            # Get coordinates of right hand
+            shoulder_r = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            elbow_r = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            wrist_r = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+            # Calculate angle
+            angle_r = calculate_angle(shoulder_r, elbow_r, wrist_r)
             
             # Visualize angle
             cv2.putText(image, str(angle), 
                            tuple(np.multiply(elbow, [640, 480]).astype(int)), 
                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                )
+                               )
             
-            # Curl counter logic
+            # Curl counter logic for left
             if angle > 160:
                 stage = "down"
             if angle < 30 and stage =='down':
                 stage="up"
                 counter +=1
-                print(counter)
-                       
+                print("Left : ",counter)
+
+            # Curl counter logic for right
+            if angle_r > 160:
+                stage_r = "down"
+            if angle_r < 30 and stage_r =='down':
+                stage_r="up"
+                counter_r +=1
+                print("Right : ",counter_r)                       
+        
         except:
             pass
         
         # Render curl counter
         # Setup status box
-        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+        cv2.rectangle(image, (0,0), (100,50), (245,117,16), -1)
         
         # Rep data
-        cv2.putText(image, 'REPS', (15,12), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter), 
-                    (10,60), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        cv2.putText(image, 'REPS', (10,10), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter_r), 
+                    (10,30), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
         
         # Stage data
-        cv2.putText(image, 'STAGE', (65,12), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage, 
-                    (60,60), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        cv2.putText(image, 'STAGE', (40,10), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage_r, 
+                    (40,30), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
         
+        
+        # Render curl counter for left hand
+        # Setup status box for left hand
+        
+        cv2.rectangle(image, (500,0), (600,50), (245,117,0), -1)
+        
+        # Rep data
+        cv2.putText(image, 'REPS', (10+500,10), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter), 
+                    (10+500,30), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        
+        # Stage data
+        cv2.putText(image, 'STAGE', (40+500,10), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage, 
+                    (40+500,30), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
         
         # Render detections
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
@@ -101,10 +133,16 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
                                  )               
         
+             
         cv2.imshow('Mediapipe Feed', image)
 
+        if counter + counter_r == 2*inputGoal:
+            print("GOOD JOB")
+            time.sleep(5)
+            break
+        
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
-    cap.release()
-    cv2.destroyAllWindows() 
+cap.release()
+cv2.destroyAllWindows() 
