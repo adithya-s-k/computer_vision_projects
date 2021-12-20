5681f5fceef55837b23e21f944c8a62f6a5aecbd
Adithya S K <adithyaskolavi@gmail.com>
2021-12-20 23:58:15 +0530

    Phase 1 of the project complete💪 - Adithya S

--- Pose_estimation_hand_curl_counter/hand_curl_counter.py
+++ Pose_estimation_hand_curl_counter/hand_curl_counter.py
@@ -11,7 +11,11 @@ cap = cv2.VideoCapture(0)
 # cap.set(3,1280)
 # cap.set(4,960)
 
-def calculate_angle(a,b,c):
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print(width, height)
+
+def calculate_angle(a,b,c):#shoulder, elbow, wrist
     a = np.array(a) # First
     b = np.array(b) # Mid
     c = np.array(c) # End
@@ -45,6 +49,8 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
+
+        
         # Extract landmarks
         try:
             landmarks = results.pose_landmarks.landmark
@@ -67,80 +73,77 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             # Visualize angle
             cv2.putText(image, str(angle), 
                            tuple(np.multiply(elbow, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                           cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (255, 255, 255), 2, cv2.LINE_AA
                                )
             
             # Curl counter logic for left
             if angle > 160:
-                stage = "down"
-            if angle < 30 and stage =='down':
-                stage="up"
+                stage = "Down"
+            if angle < 30 and stage =='Down':
+                stage="Up"
                 counter +=1
                 print("Left : ",counter)
 
             # Curl counter logic for right
             if angle_r > 160:
-                stage_r = "down"
-            if angle_r < 30 and stage_r =='down':
-                stage_r="up"
+                stage_r = "Down"
+            if angle_r < 30 and stage_r =='Down':
+                stage_r="Up"
                 counter_r +=1
                 print("Right : ",counter_r)                       
         
         except:
             pass
         
-        # Render curl counter
-        # Setup status box
-        cv2.rectangle(image, (0,0), (100,50), (245,117,16), -1)
-        
+        # Render curl counter for right hand
+        # Setup status box for right hand
+        cv2.rectangle(image, (0,0), (140,55), (245,117,16), -1)
         # Rep data
-        cv2.putText(image, 'REPS', (10,10), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter_r), 
-                    (10,30), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
-        
+        cv2.putText(image, 'REPS', (10,20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter_r), (10,45), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1, cv2.LINE_AA)
         # Stage data
-        cv2.putText(image, 'STAGE', (40,10), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage_r, 
-                    (40,30), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(image, 'STAGE', (60,20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage_r, (60,45), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1, cv2.LINE_AA)
         
         
         # Render curl counter for left hand
         # Setup status box for left hand
-        
-        cv2.rectangle(image, (500,0), (600,50), (245,117,0), -1)
-        
+        cv2.rectangle(image, (500,0), (640,55), (245,117,0), -1)# Rectange properties rectangle(image, start_point, end_point, color, thickness) 
         # Rep data
-        cv2.putText(image, 'REPS', (10+500,10), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter), 
-                    (10+500,30), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
-        
+        cv2.putText(image, 'REPS', (510,20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,0), 1, cv2.LINE_AA)#cv2.putText(image,  text_to_show,  (20, 40),  fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,  fontScale=1,  color=(255, 255, 255))
+        cv2.putText(image, str(counter), (510,45), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1, cv2.LINE_AA)
         # Stage data
-        cv2.putText(image, 'STAGE', (40+500,10), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage, 
-                    (40+500,30), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(image, 'STAGE', (560,20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage, (560,45), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1, cv2.LINE_AA)
+        
         
         # Render detections
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
                                  )               
-        
              
         cv2.imshow('Mediapipe Feed', image)
-
-        if counter + counter_r == 2*inputGoal:
+        
+        
+        #Tells is you are standing in range
+        '''if int(shoulder_r[0]) > 0.7:
+            cv2.putText(image, 'MOVE RIGHT', (100,100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 10, (0,0,0), 2, cv2.LINE_AA)
+            print("MOVE RIGHT")
+        if int(shoulder[0]) < 0.3:
+            cv2.putText(image, 'MOVE LEFT', (100,100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 10, (0,0,0), 2, cv2.LINE_AA)
+            print("MOVE LEFT")'''
+            
+        if counter == inputGoal and counter_r == inputGoal:
             print("GOOD JOB")
+            cv2.putText(image, 'GOOD JOB', (300,200), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,0), 2, cv2.LINE_AA)
             time.sleep(5)
             break
-        
+        elif counter > counter_r:
+            print("Right arm next")
+        elif counter_r > counter:
+            print("Left arm next")
+
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
