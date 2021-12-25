9037a4a1edefbd4ec6bf52d6c771c0c7af733487
Adithya S K <adithyaskolavi@gmail.com>
2021-12-25 23:50:59 +0530

    posture detection basic templete done✔ - Adithya S

---
+++ Face_detection_basic/sitting_posture_detection.py
@@ -0,0 +1,35 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import time as time
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+cap = cv2.VideoCapture(0)
+## Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+      
+        # Make detection
+        results = pose.process(image)
+    
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,0,0), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(0,245,0), thickness=2, circle_radius=2)
+                                 )               
+        
+        #if results.pose_landmarks != None:
+        
+
+    cap.release()
+    cv2.destroyAllWindows()
