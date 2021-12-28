26fcf3c3a055386942d4bb7bcd37ee90c73349c3
Adithya S K <adithyaskolavi@gmail.com>
2021-12-28 21:54:34 +0530

    basic hand detection set up done✔ - Adithya S

--- Hand_detection/basic_hand_detection.py
+++ Hand_detection/basic_hand_detection.py
@@ -1,39 +1,22 @@
-import cv2
 import mediapipe as mp
-import numpy as np
-import time as time
+import cv2
+
 mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
 mp_holistic = mp.solutions.holistic
 
 cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,960)
+cap.set(3,1000)
+cap.set(4,1000)
 
 # Initiate holistic model
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     
     while cap.isOpened():
         ret, frame = cap.read()
-        
-        # Recolor Feed
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # Make Detections
         results = holistic.process(image)
-        # print(results.face_landmarks)
-        
-        # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
-        
-        # Recolor image back to BGR for rendering
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
-        # 1. Draw face landmarks
-        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS, 
-                                 mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
-                                 mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                                 )
-        
-        # 2. Right hand
+
         mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
                                  mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
                                  mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
@@ -43,12 +26,6 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
                                  mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
                                  mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                                 )
-
-        # 4. Pose Detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
-                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                                  )
                         
         cv2.imshow('Raw Webcam Feed', image)
