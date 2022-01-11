962ab5b3fc2251ccc307ab8f01b9864fe5e86ce1
Adithya S K <adithyaskolavi@gmail.com>
2022-01-11 13:55:32 +0530

    Added Postur Detection to CounterModule - Adithya S

--- Pose_estimation/CounterModule.py
+++ Pose_estimation/CounterModule.py
@@ -476,3 +476,52 @@ def running_counter(goal_running):
                 break
 
     cv2.destroyAllWindows() 
+def posture_detector():
+    distance_cal = 1
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            results = pose.process(image)
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            try:
+                landmarks = results.pose_landmarks.landmark
+                
+                nose = [landmarks[mp_pose.PoseLandmark.NOSE.value].x,landmarks[mp_pose.PoseLandmark.NOSE.value].y]
+                # hand_r = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                rightshoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                leftshoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                midshoulder = [((rightshoulder[0] + leftshoulder[0])/2),((rightshoulder[1] + leftshoulder[1])/2)]
+                
+                middleshoulder = [landmarks[mp_pose.PoseLandmark.NOSE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                
+                # Calculate angle
+                
+                distance_cal = (calculate_distance(nose,middleshoulder)*100)
+                print(distance_cal)
+                        
+            except:
+                pass
+            
+            cv2.rectangle(image, (0,0), (1280,70), (0,0,0), -1)
+            cv2.putText(image, str(distance_cal), (10,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            
+            cv2.rectangle(image, (730,960-60), (1280,960), (0,0,0), -1)
+            if distance_cal < 25:
+                cv2.putText(image, "YOUR ARE CROUCHING", (750,960-15), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255,255,255), 2, cv2.LINE_AA)
+            else:
+                cv2.putText(image,"YOUR ARE UP STRAIGHT", (750,960-15), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255,255,255), 2, cv2.LINE_AA)
+
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                    )               
+            cv2.imshow('Mediapipe Feed', image)
+            
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+    cv2.destroyAllWindows() 
