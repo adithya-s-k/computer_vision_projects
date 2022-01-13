163aa206a62bbea581c11a2e729975760e5f8526
Adithya S K <adithyaskolavi@gmail.com>
2022-01-13 23:47:43 +0530

    Hand detection done💪 - Adithya S

--- test.py
+++ test.py
@@ -15,15 +15,10 @@ while TIMER > 0:
 
 
 while(True):
-    
-
     ret, frame = vid.read()
-  
     cv2.putText(frame, 'TIME', (560,20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,0), 1, cv2.LINE_AA)
     cv2.putText(frame, str(TIMER), (560,45), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1, cv2.LINE_AA)
-
     cv2.imshow('frame', frame)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 # After the loop release the cap object
